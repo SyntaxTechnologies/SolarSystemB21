8922f347e978b2a8a0703e0a8549d9b387a32219
SyntaxTechnologies <moazzam@syntaxtechs.com>
2025-02-19 20:04:33 -0500

    feature sun has been added

--- src/test/java/SolarSystemB21/SolarSystem.java
+++ src/test/java/SolarSystemB21/SolarSystem.java
@@ -7,6 +7,11 @@ public class SolarSystem {
         obrey.sun=1;
         obrey.moon=2;
         obrey.planets=2;
+
+        Sun exosias=new Sun();
+        exosias.color="grey";
+        exosias.temp="-54k";
+
     }
 
 }
