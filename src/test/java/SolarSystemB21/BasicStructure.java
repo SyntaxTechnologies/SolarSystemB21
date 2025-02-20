e80cba493c572877a64d7382aecd4db68a9e471e
SyntaxTechnologies <moazzam@syntaxtechs.com>
2025-02-19 19:31:37 -0500

    basic structure of the solarsystem

---
+++ src/test/java/SolarSystemB21/BasicStructure.java
@@ -0,0 +1,9 @@
+package SolarSystemB21;
+
+public class BasicStructure {
+
+    int sun;
+    int planets;
+    int moon;
+
+}
---
+++ src/test/java/SolarSystemB21/SolarSystem.java
@@ -0,0 +1,12 @@
+package SolarSystemB21;
+
+public class SolarSystem {
+
+    public static void main(String[] args) {
+        BasicStructure obrey =new BasicStructure();
+        obrey.sun=1;
+        obrey.moon=2;
+        obrey.planets=2;
+    }
+
+}
